80e34ed9b1123d98b27fdf4f4a4ff3443c9b8e44
Kelley Chaplain <kmchaplain@gmail.com>
2019-08-14 23:08:24 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,14 @@ class App < Sinatra::Base
       erb :super_hero
     end
     
-    post "/teams" do
-        @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-        members = params[:team][:members]
-        @super_heroes = members.collect do |member_params|
-          SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
-        end
-        erb :team
+    post '/teams' do
+      raise params.inspect
+        # @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        # members = params[:team][:members]
+        # @super_heroes = members.collect do |member_params|
+        #   SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+        # end
+        # erb :team
       end 
 
 
